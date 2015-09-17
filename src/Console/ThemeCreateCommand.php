6f77f192e2b55be0eacbea3f7fbc8bdf8ffc072c
Hieu Le <letrunghieu.cse09@gmail.com>
2015-09-17 13:34:50 +0700

    reecms/reecms#38 theme:create command

--- src/Console/ThemeCreateCommand.php
+++ src/Console/ThemeCreateCommand.php
@@ -3,6 +3,8 @@
 namespace Ree\Theme\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Ree\Theme\ThemeViewFinder;
 
 /**
  * Description of ThemeCreateCommand
@@ -17,7 +19,7 @@ class ThemeCreateCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'theme:create';
+    protected $signature = 'theme:create {name}';
 
     /**
      * The console command description.
@@ -27,12 +29,52 @@ class ThemeCreateCommand extends Command
     protected $description = 'Create a new theme directory structure';
 
     /**
+     * File system instance
+     *
+     * @var Filesystem
+     */
+    protected $files;
+
+    /**
+     * Theme view finder instance
+     *
+     * @var ThemeViewFinder
+     */
+    protected $finder;
+
+    public function __construct(Filesystem $files, ThemeViewFinder $finder)
+    {
+        parent::__construct();
+
+        $this->files  = $files;
+        $this->finder = $finder;
+    }
+
+    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
     {
-        $this->comment(PHP_EOL . Inspiring::quote() . PHP_EOL);
+        $theme = snake_case($this->argument('name'));
+        $this->output->writeln("Theme dir name: <info>($theme)</info>");
+
+        if ($this->finder->doesThemeExist($theme)) {
+            $this->error('Theme folder exists.');
+            return false;
+        }
+
+        $themeViewDir = $this->finder->getThemeConfig()->getThemeViewDir($theme);
+        $this->files->makeDirectory($themeViewDir, 0755, true);
+        $this->output->writeln("Creating theme views directory : <info>{$themeViewDir}</info>");
+
+        $themeAssetDir = $this->finder->getThemeConfig()->getThemeAssetDir($theme);
+        $this->files->makeDirectory($themeAssetDir, 0755, true);
+        $this->output->writeln("Creating theme assets directory: <info>{$themeAssetDir}</info>");
+
+        $this->info('DONE.');
+
+        return true;
     }
 }
